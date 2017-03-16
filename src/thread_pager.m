c300e9216b61364e6f9b550c90a2ae2c2271d416
Stian Soiland-Reyes <stain@apache.org>
2017-03-16 15:47:54 +0000

    tick for selected

--- src/thread_pager.m
+++ src/thread_pager.m
@@ -2832,7 +2832,7 @@ draw_sep(Attrs, Cols, MaybeSepPanel, !IO) :-
         MaybeSepPanel = yes(Panel),
         panel.erase(Panel, !IO),
         attr(Panel, Attrs ^ t_status ^ bar, !IO),
-        hline(Panel, char.to_int('-'), Cols, !IO)
+        hline(Panel, char.to_int('―'), Cols, !IO)
     ;
         MaybeSepPanel = no
     ).
@@ -2856,10 +2856,10 @@ draw_thread_line(TAttrs, Panel, Line, _LineNr, IsCursor, !IO) :-
 
     (
         Selected = selected,
-        mattr_draw(Panel, unless(IsCursor, Attrs ^ selected), "*", !IO)
+        mattr_draw(Panel, unless(IsCursor, Attrs ^ selected), " ✓", !IO)
     ;
         Selected = not_selected,
-        draw(Panel, " ", !IO)
+        draw(Panel, "  ", !IO)
     ),
     mattr(Panel, unless(IsCursor, Attrs ^ standard_tag), !IO),
 
